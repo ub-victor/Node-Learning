a03f0ace739c0db03d0cacdb911c0ad21c927e92
ub-victor <victoireushindi371@gmail.com>
2025-09-02 16:54:32 +0200

    Node journey

--- index.js
+++ index.js
@@ -1,65 +1,19 @@
 // Import the built-in 'fs' (File System) module provided by Node.js
 // This module lets you interact with files (read, write, delete, etc.)
-const fs = require('fs');
+const fsPromises = require('fs').promises;
 const path = require(`path`);
 
-// Asynchronously read the content of the file './files/starter.txt'
-// 1st argument: file path
-// 2nd argument: encoding type ('utf-8' means text file encoded in UTF-8 format)
-// 3rd argument: callback function that runs after the file is read
-// read file without importing the path  in const path  = require (`path`);
-// fs.readFile('./files/starter.txt', 'utf-8', (err, data) => {...});
-// let use a simplify way to read the file
-// // Build an absolute path to 'starter.txt' inside 'files' folder
-fs.readFile(path.join(__dirname, 'files', 'starter.txt'), 'utf-8', (err, data) => {
+const fileOps = async ()=> {
+    try{
+        const data = await fsPromises.readFile(path.join(__dirname, 'files', 'starter.txt'), 'utf-8');
+        console.log(data);
+        await fsPromises.writeFile(path.join(__dirname, 'files', 'promiseWrite.txt'), 'Nice to meet ya');
+        await fsPromises.appendFile(path.join(__dirname, 'files', 'promiseWrite.txt'), '\n\nYes it is');
+        await fsPromises.rename(path.join(__dirname, 'files', 'promiseWrite.txt'), path.join(__dirname, 'files', 'promiseComplete.txt'));
+        const newData = await fsPromises.readFile(path.join(__dirname, 'files', 'promiseComplete.txt'), 'utf-8');
+        console.log(newData);
 
-  // If an error occurs (for example, file not found), throw the error
-  if (err) throw err;
-
-  // If no error, log the file's content (the "data") to the console
-  console.log(data);
-
-// This line will run immediately, even before the file is finished reading,
-// because readFile is asynchronous (non-blocking).
-console.log('Hello...');
-// that is the way of writing files (path.join is used to create a path( , __dirname for absolute path , files is the folder containing the file, reply.txt is the file to be created), then the text to go in the file created (call back function)
-fs.writeFile(path.join(__dirname, 'files', 'reply.txt'), 'Nice to meet ya', (err) => {
-
-    // If an error occurs (for example, file not found), throw the error
-    if (err) throw err;
-
-    // If no error, log 'Write complete' to the console
-    console.log('Write complete');
-
-    // append file
-    fs.appendFile(path.join(__dirname, 'files', 'reply.txt'), '\n\nOh yeah yes it is', (err) => {
-
-        // If an error occurs (for example, file not found), throw the error
-        if (err) throw err;
-
-        // If no error, log 'Append complete' to the console
-        console.log('Append complete');
-
-        fs.rename(path.join(__dirname, 'files', 'reply.txt'), path.join(__dirname, 'files', 'newReply.txt'), (err) => {
-
-            // If an error occurs (for example, file not found), throw the error
-            if (err) throw err;
-
-            // If no error, log 'Rename complete' to the console
-            console.log('Rename complete');
-        });
-    });
-});
-
-// Setup a listener for "uncaughtException"
-// This runs when an error happens in your code that is not caught anywhere else
-process.on('uncaughtException', (err) => {
-    // Log the error message to the console
-    console.error('There was an uncaught error', err);
-
-    // Exit the Node.js process with code 1 (non-zero means failure)
-    process.exit(1);
-});
-
-// Add this closing brace to properly close the fs.readFile callback
-});
+    }catch(err){
+        console.error('Error occurred:', err);
+    }
+}
