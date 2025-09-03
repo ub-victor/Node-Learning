51424870705bf9f2b7222b36dd071b16bdf064c7
ub-victor <victoireushindi371@gmail.com>
2025-09-03 12:47:12 +0200

    Node journey

--- dir.js
+++ dir.js
@@ -1,10 +1,18 @@
 const fs = require('fs');
 
 
-if(!fs.existsSync('./new')){ // checks if a file or directory already exists.
+if(!fs.existsSync('./new')){ // checks if a file or directory does not already exist.
     fs.mkdirSync('./new', (err)=>{
        if (err) throw err;
        console.log('Directory created successfully!');
    });
 }
 
+
+// to remove a directory
+if(fs.existsSync('./new')){ // checks if a file or directory already exists.
+    fs.rmdir('./new', (err)=>{
+       if (err) throw err;
+       console.log('Directory removed successfully!');
+   });
+}
