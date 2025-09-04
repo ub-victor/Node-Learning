4ac4fbe5c931e141afe0a259c1a461d0829798e7
ub-victor <victoireushindi371@gmail.com>
2025-09-04 16:15:35 +0200

    Node journey

--- npm/server.js
+++ npm/server.js
@@ -1,17 +1,30 @@
-const logEvent = require('./logEvent');  // Import the custom logEvent function
+const http = require('http');
+const path = require('path');
+const fs = require('fs');
+const fsPromises = require('fs').promises;
 
-const EventEmitter = require('events');  // Import Node.js EventEmitter
+const logEvent = require('./logEvent');
+const EventEmitter = require('events');  
+const { console } = require('inspector');
+class Emitter extends EventEmitter { };
+const myEmitter = new Emitter();
 
-// Create a custom class that extends EventEmitter
-class MyEmitter extends EventEmitter {};
+const PORT = process.env.PORT || 3500;
 
-// Create an instance of the custom event emitter
-const myEmitter = new MyEmitter();
+const server = http.createServer((req, res) => {
+    console.log(req.url, req.method);
+});
 
-// Listen for the 'log' event and handle it by calling logEvent
+server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
+
+
+
+
+
+/*
 myEmitter.on('log', (msg) => logEvent(msg));
 
-// After 2 seconds, emit the 'log' event with different messages
-setTimeout(() => {
     myEmitter.emit('log', 'Log message 1');
-}, 2000);
+;
+*/
