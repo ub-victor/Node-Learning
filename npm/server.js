38dd827c7c423292e7d6e3f35e61ca63f0f849c2
ub-victor <victoireushindi371@gmail.com>
2025-09-09 15:48:09 +0200

    Node In

--- npm/server.js
+++ npm/server.js
@@ -14,14 +14,23 @@ const server = http.createServer((req, res) => {
     console.log(req.url, req.method);
 
     let path;
-     if(req.url === '/' || req.url === 'index.html'){
-         res.statusCode = 200;
-         res.setHeader('Content-Type', 'text/html');
-         path = path.join(__dirname, 'views', 'index.html');
-         fs.readFile(path, 'utf8', (err, data)=>{
-             res.end(data); 
-         })
-     } 
+       if(req.url === '/' || req.url === 'index.html'){
+           res.statusCode = 200;
+           res.setHeader('Content-Type', 'text/html');
+           path = path.join(__dirname, 'views', 'index.html');
+           fs.readFile(path, 'utf8', (err, data)=>{
+               res.end(data); 
+           })
+        } 
+
+       if(req.url === '/' || req.url === 'index.html'){
+           res.statusCode = 200;
+           res.setHeader('Content-Type', 'text/html');
+           path = path.join(__dirname, 'views', 'index.html');
+           fs.readFile(path, 'utf8', (err, data)=>{
+               res.end(data); 
+           })
+        }  
 }); 
 
 server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
