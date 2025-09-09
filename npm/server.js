01aed88e1032623699fa1ed0034b3a4594e70101
ub-victor <victoireushindi371@gmail.com>
2025-09-09 16:59:22 +0200

    Node In

--- npm/server.js
+++ npm/server.js
@@ -16,6 +16,29 @@ const server = http.createServer((req, res) => {
 
     let contentType;
 
+    switch (extension) {
+        case '.css':
+            contentType = 'text/css';
+            break;
+        case '.js':
+            contentType = 'text/javascript';
+            break;
+        case '.json':
+            contentType = 'application/json';
+            break;
+        case '.jpg':
+            contentType = 'image/jpeg';
+            break;
+        case '.png':
+            contentType = 'image/png';
+            break;
+        case '.txt':
+            contentType = 'text/plain';
+            break;
+        default:
+            contentType = 'text/html';
+    }
+
 
 
     
