1deec47b540f876090ef533ee11c56b04123a02b
ub-victor <victoireushindi371@gmail.com>
2025-09-09 16:57:05 +0200

    Node In

--- npm/server.js
+++ npm/server.js
@@ -12,25 +12,13 @@ const PORT = process.env.PORT || 3500;
 
 const server = http.createServer((req, res) => {
     console.log(req.url, req.method);
+    const extension = path.extname(req.url);
 
-    let path;
-       if(req.url === '/' || req.url === 'index.html'){
-           res.statusCode = 200;
-           res.setHeader('Content-Type', 'text/html');
-           path = path.join(__dirname, 'views', 'index.html');
-           fs.readFile(path, 'utf8', (err, data)=>{
-               res.end(data); 
-           })
-        } 
 
-       if(req.url === '/' || req.url === 'index.html'){
-           res.statusCode = 200;
-           res.setHeader('Content-Type', 'text/html');
-           path = path.join(__dirname, 'views', 'index.html');
-           fs.readFile(path, 'utf8', (err, data)=>{
-               res.end(data); 
-           })
-        }  
+
+    
+        
+
 }); 
 
 server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
