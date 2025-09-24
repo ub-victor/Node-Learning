e6129f1bae1fbf5c6291b3515d59cb9d1d3c7d9e
ub-victor <victoireushindi371@gmail.com>
2025-09-24 16:30:22 +0200

    Node cors

--- npm/server.js
+++ npm/server.js
@@ -60,7 +60,7 @@ app.get(/\/*/, (req, res) => {
 });
 
 app.use(function (err, req, res, next) {
-    console.error(err.stack);
+    console.error(err.stack); 
     res.status(500).send(err.message);
 });
 
