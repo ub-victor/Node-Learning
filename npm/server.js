3ebb2b02ecaf169cceddf5e129d4e46bc89b0de8
ub-victor <victoireushindi371@gmail.com>
2025-09-26 14:53:53 +0200

    Node

--- npm/server.js
+++ npm/server.js
@@ -33,9 +33,12 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
 //serve static files
+// default path is /public
+// app.use('/', express.static(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'public')));
+app.use('/subdir', express.static(path.join(__dirname, 'public')));
 
-
+app.use('/subdir', require('./routes/subdir'));
 
 app.get(/^\/$|\/index(.html)?$/, (req, res) => {
     res.sendFile(path.join(__dirname, 'views', 'index.html'));
