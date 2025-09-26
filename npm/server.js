6dc015241d1372edd744acd1860030236ee84aba
ub-victor <victoireushindi371@gmail.com>
2025-09-26 15:00:51 +0200

    Node

--- npm/server.js
+++ npm/server.js
@@ -38,28 +38,10 @@ app.use(express.json());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use('/subdir', express.static(path.join(__dirname, 'public')));
 
+
+app.use('/', require('./routes/root'));
 app.use('/subdir', require('./routes/subdir'));
 
-app.get(/^\/$|\/index(.html)?$/, (req, res) => {
-    res.sendFile(path.join(__dirname, 'views', 'index.html'));
-});
-
-app.get(/new-page(.html)?/, (req, res) => {
-    res.sendFile(path.join(__dirname, 'views', 'new-page.html'));
-});
-
-app.get(/old-page(.html)?/, (req, res) => {
-    res.redirect(301, '/new-page.html');
-});
-
-// Route handlers
-
-app.get(/\/hello(.html)?/, (req, res, next) => {
-    console.log('Attempted to load hello.html');
-    next()
-}, (req, res) => {
-    res.send('Hello World!');
-});
 
 app.get(/\/*/, (req, res) => {
     res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
