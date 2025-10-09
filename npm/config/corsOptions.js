f7a3a046ca4b556a4153c0e818b98b054315c6c5
ub-victor <victoireushindi371@gmail.com>
2025-10-09 12:31:53 +0200

    Server with cont

--- npm/config/corsOptions.js
+++ npm/config/corsOptions.js
@@ -1,2 +1,17 @@
+const { useCallback } = require("react")
+
 const whitelist = [
-    'https://www.yoursite.com', 'http://127.0.0.1:5500', 'http://localhost:3500']
+    'https://www.yoursite.com',
+    'http://127.0.0.1:5500', 
+    'http://localhost:3500']
+
+    const corsOptions = {
+        origin: (origin, callback) => {
+            if (whitelist.indexOf(origin) !== -1 || !origin) {
+                callback(null, true);
+            } else {
+                callback(new Error('Not allowed by CORS'));
+            }
+        },
+        optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+    }
