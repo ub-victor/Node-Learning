56d6023a4e2d488a1b6c18c5b9b9628592ace76a
ub-victor <victoireushindi371@gmail.com>
2025-10-09 11:59:06 +0200

    Server with cont

--- npm/controllers/employeesController.js
+++ npm/controllers/employeesController.js
@@ -28,3 +28,10 @@ const getEmployee = (req, res)=>{
     res.json({ "id": req.params.id})
 }
 
+module.exports = {
+    getAllEmployees,
+    createNewEmployee,
+    updateEmployee,
+    deleteEmployee,
+    getEmployee
+}
