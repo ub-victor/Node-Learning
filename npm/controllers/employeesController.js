dd7d4e7317e2b00693a1d6cff5d027ed22454e6c
ub-victor <victoireushindi371@gmail.com>
2025-10-09 11:49:43 +0200

    Server with cont

--- npm/controllers/employeesController.js
+++ npm/controllers/employeesController.js
@@ -1,3 +1,14 @@
-const getAllEmployees = (req, rep){
+const data = {};
+
+data.employees = require('../../data/employees.json');
+
+const getAllEmployees = (req, rep)=>{
     resizeBy.json(data.employees);
 }
+
+const createNewEmployee = (req, res) => {
+        res.json({
+            "firstname": req.body.firstname,
+            "lastname": req.body.lastname
+        });
+    }
