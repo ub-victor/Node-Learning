b8b37ebf93ad9babedb062bc6c020f02dfb7973d
ub-victor <victoireushindi371@gmail.com>
2025-10-09 12:02:07 +0200

    Server with cont

--- npm/routes/api/employees.js
+++ npm/routes/api/employees.js
@@ -4,6 +4,7 @@ const router = express.Router();
 const data = {};
 
 data.employees = require('../../data/employees.json');
+const getEmployeesController = require('../../controllers/employeesController')
 
 // routers
 
