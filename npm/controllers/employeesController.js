8b83f925c711a15753e0d21a53466482fa6eecf4
ub-victor <victoireushindi371@gmail.com>
2025-10-20 19:48:41 +0200

    Node MVC

--- npm/controllers/employeesController.js
+++ npm/controllers/employeesController.js
@@ -31,7 +31,7 @@ const updateEmployee = (req, res)=>{
     if(req.body.lastname) employee.lastname = req.lastname;
     const filterArray =  data.employees.filter (emp => emp.id !== parseInt(req.body.id));
     const unsortedArray = [... filterArray, employee];
-    data.setEmployees(unsortedArray.sort((a,b)=> a.id > b.id ? 1 : a.id < b.id ? -1 : 0))
+    data.setEmployees(unsortedArray.sort((a,b)=> a.id > b.id ? 1 : a.id < b.id ? -1 : 0));
     res.json(data.employees);
 
 }
