b66ac1663a2bd1578f32453e36c4afc2d2b67539
ub-victor <victoireushindi371@gmail.com>
2025-10-22 16:46:41 +0200

    MVC

--- npm/controllers/employeesController.js
+++ npm/controllers/employeesController.js
@@ -3,7 +3,7 @@ const data = {
     setEmployees: function(data){this.employees = data }
 };
 
-const getAllEmployees = (req, rep)=>{
+const getAllEmployees = (req, res)=>{
     res.json(data.employees);
 }
 
