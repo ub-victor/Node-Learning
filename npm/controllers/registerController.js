1374fce0c32067fbba84c5998d11d9be66f68ff9
ub-victor <victoireushindi371@gmail.com>
2025-10-23 17:59:00 +0200

    MVC

--- npm/controllers/registerController.js
+++ npm/controllers/registerController.js
@@ -6,4 +6,12 @@ const usersDB = {
 }
 
 const fsPromises = require('fs').promises;
-const path = require('path')
+const path = require('path');
+const bcrypt = require('bcrypt');
+
+
+const handleNewUser = async (req, res)=>{
+    const {user, pwd} = req.body;
+    if (!user || !pwd)
+
+}
