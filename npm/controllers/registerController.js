d50a484f901dfd56e351caadca142b7b3ceedbd3
ub-victor <victoireushindi371@gmail.com>
2025-10-28 09:03:11 +0200

    MVC

--- npm/controllers/registerController.js
+++ npm/controllers/registerController.js
@@ -23,9 +23,13 @@ const handleNewUser = async (req, res)=>{
         //store the new user
         const newUser = {"username": user, "password": hashedPwd }
         usersDB.setUsers([...usersDB.users, newUser]);
-        await.fsPromi
+        await fsPromises.writeFile(
+            path.join(__dirname, '..', 'model', 'users.json'),
+            JSON.stringify(usersDB.users)
+        );
 
-
+        console.log(usersDB.users);
+        res.status(201).json({'success': `New user ${user} created!` });
     }catch (err){
         res.status(500).json({'message': err.message});
     }
