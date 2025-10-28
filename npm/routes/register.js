295b4b630855d74e0cd30fe66ae4645a0e407d24
ub-victor <victoireushindi371@gmail.com>
2025-10-28 10:10:50 +0200

    MVC

--- npm/routes/register.js
+++ npm/routes/register.js
@@ -2,3 +2,4 @@ const express = require('express');
 const router = express.Router();
 const registerController = require('../controllers/registerController');
 
+router.post('/', registerController.handleNewUser);
