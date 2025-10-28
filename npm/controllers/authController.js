e3033e3bf69edb8fb5a91a2441ec97d25218a3a8
ub-victor <victoireushindi371@gmail.com>
2025-10-28 11:41:23 +0200

    MVC

--- npm/controllers/authController.js
+++ npm/controllers/authController.js
@@ -7,4 +7,6 @@ const bcrypt = require('bcrypt')
 
 const handleLogin = async (req, res)=> {
     const {user, pwd} = req.body;
+    if(!user || !pwd) return res.status(400).json({'message': 'Username and password ares required.'});
 }
+
