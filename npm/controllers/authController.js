c7f0e58c5562e3860102d8d3baff278a9099b523
ub-victor <victoireushindi371@gmail.com>
2025-10-28 11:56:30 +0200

    MVC

--- npm/controllers/authController.js
+++ npm/controllers/authController.js
@@ -9,6 +9,7 @@ const handleLogin = async (req, res)=> {
     const {user, pwd} = req.body;
     if(!user || !pwd) return res.status(400).json({'message': 'Username and password ares required.'});
     const foundUser = usersDB.users.find(person => person.username === user);
+    if(!foundUser) return res.sendStatus(401); // Unauthorized
     // evaluate password
     const match = await bcrypt.compare(pwd, foundUser.password);
     if(match){
