2b612acd1897f4bf241cca2e82f67dec9e4c832d
ub-victor <victoireushindi371@gmail.com>
2025-11-01 00:56:19 +0200

    JWT

--- npm/controllers/authController.js
+++ npm/controllers/authController.js
@@ -8,6 +8,7 @@ const bcrypt = require('bcrypt');
 const jtw = require('jsonwebtoken')
 require('dotenv').config();
 const fsPromises = require ('fs').promises;
+const path = require('path');
 
 const handleLogin = async (req, res)=> {
     const {user, pwd} = req.body;
