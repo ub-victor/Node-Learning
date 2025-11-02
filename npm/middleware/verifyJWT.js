6cda13996076e05a9ae791d41a1c2f42819e61ab
ub-victor <victoireushindi371@gmail.com>
2025-11-02 22:50:50 +0200

    JWT

--- npm/middleware/verifyJWT.js
+++ npm/middleware/verifyJWT.js
@@ -5,7 +5,7 @@ const verifyJWT = (req, res, next)=>{
     const authHeader = req.headers['authorization'];
     if(!authHeader) return res.sendStatus(401);
     console.log(authHeader); // Bearer token
-    const token = authHeader.split(' ')[1]
+    const token = authHeader.split(' ')[1];
     jwt.verify(
         token,
         process.env.ACCESS_TOKEN_SECRET,
