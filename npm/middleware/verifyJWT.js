e4366412793b7277ef452dd928022269efdc2266
ub-victor <victoireushindi371@gmail.com>
2025-11-02 22:28:20 +0200

    JWT

--- npm/middleware/verifyJWT.js
+++ npm/middleware/verifyJWT.js
@@ -1,7 +1,7 @@
 const jwt = require('jsonwebtoken');
 require('dotenv').config();
 
-const verifyJWT = (req, res, next)={
+const verifyJWT = (req, res, next)=>{
     const authHeader = req.headers['authorization'];
     if(!authHeader) return res.sendStatus(401);
     console.log(authHeader); // Bearer token
