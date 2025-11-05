626cd3e742634d01f6b81aafc4683ad66fd22492
ub-victor <victoireushindi371@gmail.com>
2025-11-05 16:01:38 +0200

    JWT

--- npm/controllers/refreshTokenController.js
+++ npm/controllers/refreshTokenController.js
@@ -7,7 +7,7 @@ const usersDB = {
 const jwt = require('jsonwebtoken');
 require('dotenv').config();
 
-const handleLogin = async (req, res)=> {
+const handleRefreshToken = async (req, res)=> {
     const {user, pwd} = req.body;
     if(!user || !pwd) return res.status(400).json({'message': 'Username and password are required.'});
     const foundUser = usersDB.users.find(person => person.username === user);
