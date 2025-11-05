95f0187b354e230b071e1cbc3ebe11e77dd02f83
ub-victor <victoireushindi371@gmail.com>
2025-11-05 15:59:02 +0200

    JWT

--- npm/server.js
+++ npm/server.js
@@ -22,7 +22,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
 // Middleware for cookies
-app.use(cookieParser());
+app.use(cookieParser()); // this will parse cookies from the request headers
 
 // Serve static files (default path is /public)
 app.use(express.static(path.join(__dirname, '/public')));
