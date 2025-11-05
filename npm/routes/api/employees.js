3d05e7b6627a15f8ab82def6138237dcf2fc8de3
ub-victor <victoireushindi371@gmail.com>
2025-11-05 15:40:56 +0200

    JWT

--- npm/routes/api/employees.js
+++ npm/routes/api/employees.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const router = express.Router();
 const employeesController = require('../../controllers/employeesController');
-const verifyJWT = require('../../middleware/verifyJWT');
 
 router.route('/')
     .get(employeesController.getAllEmployees)
