a316f41c2b77bd0beaab9408037abe908882a9d4
ub-victor <victoireushindi371@gmail.com>
2025-11-08 01:13:33 +0200

    JWT

--- npm/controllers/refreshTokenController.js
+++ npm/controllers/refreshTokenController.js
@@ -9,7 +9,7 @@ require('dotenv').config();
 
 const handleRefreshToken = (req, res)=> {
     const cookies = req.cookies;
-    if(!cookies?.jwt) return res.status(401) // 401 = Unauthorized, and this line checks if the cookie named 'jwt' exists in the request cookies
+    if(!cookies?.jwt) return res.sendStatus(401) // 401 = Unauthorized, and this line checks if the cookie named 'jwt' exists in the request cookies
     console.log(cookies.jwt); 
     const refreshToken = cookies.jwt;
 
