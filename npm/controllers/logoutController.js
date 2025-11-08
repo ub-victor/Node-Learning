dae3c15ed622467e909d4f18e204fd18d5825f53
ub-victor <victoireushindi371@gmail.com>
2025-11-08 02:12:40 +0200

    JWT

--- npm/controllers/logoutController.js
+++ npm/controllers/logoutController.js
@@ -5,6 +5,7 @@ const usersDB = {
 
 const fsPromises = require ('fs').promises;
 const path = require('path');
+const { use } = require('react');
 
 const handleLogout = (req, res)=> {
     // On client, also delete the access token
@@ -24,11 +25,9 @@ const handleLogout = (req, res)=> {
     }
     // Remove refreshToken in DB
     const otherUsers = usersDB.users.filter(person => person.refreshToken !== refreshToken);
-    usersDB.setUsers([...otherUsers]);
-    await fsPromises.writeFile(
-        path.join(__dirname, '..', 'model', 'users.json'),
-        JSON.stringify(usersDB.users)
-    );
+    const currentUser = { ...foundUser, refreshToken: ''}; // remove refreshToken from current user
+    usersDB.setUsers([...otherUsers, currentUser])
+    
 
     res.clearCookie('jwt', {httpOnly : true}); // secure: true means the cookie will only be sent over HTTPS
     res.sendStatus(204);
