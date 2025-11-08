6214cf79fe87d78fcfac75081e4bcedc90712b20
ub-victor <victoireushindi371@gmail.com>
2025-11-08 02:00:24 +0200

    JWT

---
+++ npm/controllers/logoutController.js
@@ -0,0 +1,37 @@
+const usersDB = {
+    users: require('../model/users.json'),
+    setUsers: function(data){this.users = data}
+}
+
+const fsPromises = require ('fs').promises;
+const path = require('path');
+
+const handleRefreshToken = (req, res)=> {
+    const cookies = req.cookies;
+    if(!cookies?.jwt) return res.sendStatus(401) // 401 = Unauthorized, and this line checks if the cookie named 'jwt' exists in the request cookies
+    console.log(cookies.jwt); 
+    const refreshToken = cookies.jwt;
+
+    const foundUser = usersDB.users.find(person => person.refreshToken === refreshToken);
+    if(!foundUser) return res.sendStatus(403); // 403 = Forbidden;
+    // evaluate jwt
+
+    // verify the validity of the refresh token, the jwt.verify method takes the token, the secret key, and a callback function as arguments with the purpose of decoding the token and checking its validity
+    jwt.verify( 
+        refreshToken, // the token to be verified
+        process.env.REFRESH_TOKEN_SECRET,  // the secret key used to sign the token
+        (err, decoded) => { // callback function that runs after verification
+            if(err || foundUser.username !== decoded.username) return res.sendStatus(403);
+            const accessToken = jwt.sign( // create new access token
+                {"username": decoded.username}, // payload containing the username
+                process.env.ACCESS_TOKEN_SECRET, // secret key for signing the token
+                {expiresIn: '30s' }
+            );
+            res.json({ accessToken }); // send the new access token as a JSON response
+        } 
+    );
+
+       
+}
+
+module.exports = { handleRefreshToken };
