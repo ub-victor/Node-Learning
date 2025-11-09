648bdff2ec554b335350290f6c73164806104963
ub-victor <victoireushindi371@gmail.com>
2025-11-09 23:51:24 +0200

    Role in Node

--- npm/routes/api/employees.js
+++ npm/routes/api/employees.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const router = express.Router();
 const employeesController = require('../../controllers/employeesController');
+const ROLES_LIST = require('../../config/roles_list');
+
+const verifyRole = require('../../middleware/verifyRole');
 
 router.route('/')
     .get(employeesController.getAllEmployees)
