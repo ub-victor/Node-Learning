cd9e25d6832fb96460c11baa4cd6362e3c740977
ub-victor <victoireushindi371@gmail.com>
2025-11-09 23:11:47 +0200

    Role in Node

--- npm/controllers/registerController.js
+++ npm/controllers/registerController.js
@@ -29,7 +29,10 @@ const handleNewUser = async (req, res)=>{
         */
         const hashedPwd = await bcrypt.hash(pwd, 10); 
         //store the new user
-        const newUser = {"username": user, "password": hashedPwd }
+        const newUser = {
+            "username": user,
+             "password": hashedPwd
+             }
         usersDB.setUsers([...usersDB.users, newUser]);
         await fsPromises.writeFile(
             path.join(__dirname, '..', 'model', 'users.json'),
