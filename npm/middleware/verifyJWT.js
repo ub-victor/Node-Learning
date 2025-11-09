0e7d729b26e78f90c9d26fcd9a9d8c3d56db0cab
ub-victor <victoireushindi371@gmail.com>
2025-11-09 23:40:39 +0200

    Role in Node

--- npm/middleware/verifyJWT.js
+++ npm/middleware/verifyJWT.js
@@ -4,7 +4,6 @@ require('dotenv').config();
 const verifyJWT = (req, res, next)=>{
     const authHeader = req.headers.authorization || req.headers.Authorization; // Bearer token
     if(!authHeader?.startsWith('Bearer ')) return res.sendStatus(401);
-    console.log(authHeader); // Bearer token
     const token = authHeader.split(' ')[1];
     jwt.verify(
         token,
