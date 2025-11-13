8bc23e1b3a24ee7ca1211b133ff66394713a1e93
ub-victor <victoireushindi371@gmail.com>
2025-11-13 19:16:55 +0200

    MoongoDB chema

--- npm/model/Employee.js
+++ npm/model/Employee.js
@@ -2,4 +2,15 @@ const mongoose = require('mongoose'); // Import mongoose library to define schem
 
 const schema = mongoose.Schema; // Define a shorthand for mongoose.Schema
 
-const 
+const employeeSchema = new schema({
+    firstname: {
+        type: String,
+        required: true
+    },
+    lastname: {
+        type: String,
+        required: true
+    }
+});
+
+module.exports = mongoose.model('Employee', employeeSchema);
