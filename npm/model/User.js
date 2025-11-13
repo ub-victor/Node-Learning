34024ba86836ce03403d68cedbc78f3890043d24
ub-victor <victoireushindi371@gmail.com>
2025-11-13 19:32:59 +0200

    MoongoDB chema

--- npm/model/User.js
+++ npm/model/User.js
@@ -1,3 +1,15 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
+const userSchema = new Schema ({
+    username: {
+        type: String,
+        require: true
+    },
+    roles: {
+        User: {
+            type: Number,
+            default: 2001
+        }
+    }
+})
