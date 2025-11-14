937f5685285cba721d5b001e28bc85a673e1694e
ub-victor <victoireushindi371@gmail.com>
2025-11-15 01:43:10 +0200

    Configure the bd

--- npm/controllers/refreshTokenController.js
+++ npm/controllers/refreshTokenController.js
@@ -1,9 +1,4 @@
-const usersDB = {
-    users: require('../model/users.json'),
-    setUsers: function(data){this.users = data}
-}
-
-
+const User = require('../model/User');
 const jwt = require('jsonwebtoken');
 
 const handleRefreshToken = async (req, res)=> {
