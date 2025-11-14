055108fa1add8530ab9580110c6afc970d76e522
ub-victor <victoireushindi371@gmail.com>
2025-11-15 00:31:23 +0200

    Configure the bd

--- npm/controllers/registerController.js
+++ npm/controllers/registerController.js
@@ -28,11 +28,11 @@ const handleNewUser = async (req, res)=>{
         */
         const hashedPwd = await bcrypt.hash(pwd, 10); 
         //store the new user
-        const newUser = {
+        const result = await User.create({
             "username": user,
             "roles": { "User": 2001 },
              "password": hashedPwd
-             }
+        })
         
         res.status(201).json({'success': `New user ${user} created!` });
     }catch (err){
