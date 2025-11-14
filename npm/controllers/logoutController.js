5e006d28d2a3fcfa0ea38770012cc2dca5d059ac
ub-victor <victoireushindi371@gmail.com>
2025-11-15 01:41:02 +0200

    Configure the bd

--- npm/controllers/logoutController.js
+++ npm/controllers/logoutController.js
@@ -3,9 +3,6 @@ const usersDB = {
   setUsers: function (data) { this.users = data; }
 };
 
-const fsPromises = require('fs').promises;
-const path = require('path');
-
 const handleLogout = async (req, res) => {
   try {
     const cookies = req.cookies;
