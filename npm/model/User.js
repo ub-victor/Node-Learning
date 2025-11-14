d9e7caf1dfcc012e7d3a262b1229a82ff7cfa276
ub-victor <victoireushindi371@gmail.com>
2025-11-14 19:23:11 +0200

    Mongoose

--- npm/model/User.js
+++ npm/model/User.js
@@ -5,7 +5,7 @@ const Schema = mongoose.Schema;
 const userSchema = new Schema ({
     username: {
         type: String,
-        require: true
+        required: true
     },
     roles: {
         User: {
