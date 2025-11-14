184a05c6c769f1de3c78486d3ab44da881c2e874
ub-victor <victoireushindi371@gmail.com>
2025-11-15 01:53:28 +0200

    json to mongo conf

--- npm/controllers/authController.js
+++ npm/controllers/authController.js
@@ -5,7 +5,7 @@ const jwt = require('jsonwebtoken');
 const handleLogin = async (req, res)=> {
     const {user, pwd} = req.body;
     if(!user || !pwd) return res.status(400).json({'message': 'Username and password are required.'});
-    const foundUser = usersDB.users.find(person => person.username === user);
+    const foundUser = await User.findOne({ username: user}).exec();
     if(!foundUser) return res.sendStatus(401); // Unauthorized
     // evaluate password
     const match = await bcrypt.compare(pwd, foundUser.password);
