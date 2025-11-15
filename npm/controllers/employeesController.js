6c13e779567fe806e1698ab968b81753ff85b2b5
ub-victor <victoireushindi371@gmail.com>
2025-11-15 04:01:19 +0200

    json to mongo conf

--- npm/controllers/employeesController.js
+++ npm/controllers/employeesController.js
@@ -24,13 +24,13 @@ const createNewEmployee = async (req, res) => {
 }
 
 const updateEmployee = async (req, res) => {
-     if(!req?.body?.id) return res.status(400).json({"message": "ID paremeter is required"});
-     
-     const employee = await Employee.findOne({_id: req.body.id}).exec()
+    if (!req?.body?.id) {
+        return res.status(400).json({ 'message': 'ID parameter is required.' });
+    }
 
-
+    const employee = await Employee.findOne({ _id: req.body.id }).exec();
     if (!employee) {
-        return res.status(204).json({ "message": `No employee matches ID ${req.body.id}` });
+        return res.status(204).json({ "message": `No employee matches ID ${req.body.id}.` });
     }
     if (req.body?.firstname) employee.firstname = req.body.firstname;
     if (req.body?.lastname) employee.lastname = req.body.lastname;
@@ -39,13 +39,13 @@ const updateEmployee = async (req, res) => {
 }
 
 const deleteEmployee = async (req, res) => {
-    if(!req?.body?.id)return res.status(400).json({"message": "Employee ID required"});
-    const employee = await Employee.findOne({_id: req.body.id}).exec();
+    if (!req?.body?.id) return res.status(400).json({ 'message': 'Employee ID required.' });
 
+    const employee = await Employee.findOne({ _id: req.body.id }).exec();
     if (!employee) {
-        return res.status(204).json({ "message": `No employee matches ID ${req.body.id}` });
+        return res.status(204).json({ "message": `No employee matches ID ${req.body.id}.` });
     }
-    const result = await employee.deleteOne({_id: req.body.id});
+    const result = await employee.deleteOne(); //{ _id: req.body.id }
     res.json(result);
 }
 
