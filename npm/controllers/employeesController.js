4b7d0b7c955e45ce795b54eacacf40536d3789bf
ub-victor <victoireushindi371@gmail.com>
2025-11-15 02:29:30 +0200

    json to mongo conf

--- npm/controllers/employeesController.js
+++ npm/controllers/employeesController.js
@@ -11,6 +11,14 @@ const createNewEmployee = async (req, res) => {
         return res.status(400).json({ 'message': 'First and last names are required'});
     }
 
+    try {
+        const result = await Employee.create({
+            firstname: req.body.firstname,
+            lastname: req.body.lastname
+        })
+        res.status(201).json(result);
+    }
+
 
 }
 
