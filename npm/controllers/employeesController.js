c40b75288ecc7e6dafe3d2a504afb3c405b7c774
ub-victor <victoireushindi371@gmail.com>
2025-11-15 02:16:43 +0200

    json to mongo conf

--- npm/controllers/employeesController.js
+++ npm/controllers/employeesController.js
@@ -2,6 +2,7 @@ const Employee = require ("../model/Employee")
 
 const getAllEmployees = async (req, res) => {
     const employees = await Employee.find();
+    if(!employees) return res.status(204).json({'message': 'No employees found.'});
 }
 
 const createNewEmployee = (req, res) => {
